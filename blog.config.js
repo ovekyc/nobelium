08396f3353ef9f2316e11b46412bc2ea7f89c0b5
Youngchan Kim <ovekyc@gmail.com>
2022-07-11 20:32:13 +0100

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'e89chan@gmail.com',
   link: '',
   description: '',
-  lang: ['en-US', 'ko-KR'],
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
