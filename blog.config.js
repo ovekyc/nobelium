f211e80aa89be57adde6c10fb0b2b2d40a32222d
Youngchan Kim <ovekyc@gmail.com>
2022-07-17 08:30:20 +0100

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -39,7 +39,7 @@ const BLOG = {
     // support provider: gitalk, utterances, cusdis
     provider: 'gitalk', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
-      repo: 'nobelium-comments', // The repository of store comments
+      repo: 'nobelium', // The repository of store comments
       owner: 'ovekyc',
       admin: ['ovekyc'],
       clientID: '06e94722e0e4ed61620f',
